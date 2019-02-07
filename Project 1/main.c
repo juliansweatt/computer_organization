bb481d7f99fc867aba04d18e1f3507d826484758
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-02-07 15:12:06 -0500

    Initialize Main File

---
+++ Project 1/main.c
@@ -0,0 +1,12 @@
+/* @file main.c
+ * @brief Main program for the MIPS assembler.
+ * @author Julian Sweatt
+ */
+
+/**
+ * @brief The main function for the assembler. 
+ */
+int main()
+{
+    return(0);
+} 
